97df3e59eaf093688c12e24dfebb5a81b9c2ce84
yessyvasquez632-gif <yessyvasquez632@gmail.com>
2026-02-26 13:01:20 -0500

    adding python project

--- Desktop/Yessy's dae project/python_1/firstPythonProject/print ("A welcome messege").py
+++
@@ -1,6 +0,0 @@
-print ("Display a welcome messege")
-#user press 1 to roll the die 
-userRollResponseString=input (" press 1 to roll the die")
-if 1+3==2:
-  print("Yes it's correct")
-  print("End statement")
